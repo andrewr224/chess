3d1843fdc3a220fce6493aac9df91de161151271
andrewr224 <andrew.r224@gmail.com>
2017-09-28 14:00:53 +0300

    Player#select_a_square can take and validate user input

--- lib/player.rb
+++ lib/player.rb
@@ -4,11 +4,23 @@ class Player
     @color = color
   end
 
-  def change_to_coordinates(square)
-    square = square.dup.split("")
-    char = square[0]
+  def select_a_square
+    input = gets.chomp
+    return false if input.length != 2
+
+    square = input.dup.split("")
+
+    col = change_to_number(square[0].downcase)
     num = square[1].to_i
-    char = case char
+
+    return false unless col
+    return false unless (1..8).include? num
+
+    [col, num]
+  end
+
+  def change_to_number(char)
+    col = case char
     when 'a'
       1
     when 'b'
@@ -25,8 +37,8 @@ class Player
       7
     when 'h'
       8
+    else
+      false
     end
-
-    [char, num]
   end
 end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -3,6 +3,8 @@ require 'chess'
 RSpec.describe "Chess" do
   let(:game) { Chess.new }
   let(:board) { game.board }
+  let(:white) { game.players.first }
+
   it "is a game" do
     expect(game).to be_instance_of Chess
   end
@@ -29,6 +31,24 @@ RSpec.describe "Chess" do
       board.add_piece(Pawn.new(:white, [2,2]))
       board.remove_piece([2,2])
       expect(board.squares[[2,2]]).to be_nil
+    end
+  end
+
+  describe 'Player' do
+    $stdin = File.open("./spec/test.txt", "r")
+    it 'can select a square' do
+      expect(white.select_a_square).to eq([1,1])
+      expect(white.select_a_square).to eq([2,4])
+    end
+
+    it 'cannot select a square that is not on the board' do
+      expect(white.select_a_square).to be_falsey
+      expect(white.select_a_square).to be_falsey
+    end
+
+    it 'cannot accept invalid input' do
+      expect(white.select_a_square).to be_falsey
+      expect(white.select_a_square).to be_falsey
     end
   end
 
