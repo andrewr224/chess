031ea324c3acd37d0b48365b1f79c0eba0c7f196
andrewr224 <andrew.r224@gmail.com>
2017-09-29 10:10:14 +0300

    Spec accounts for change to Piece#validate_move

--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -3,26 +3,21 @@ require 'chess'
 RSpec.describe 'Pieces' do
 
   describe 'King' do
-    let(:white_king) { King.new(:white, [5,1]) }
+    let(:white_king) { King.new(:white, [5,2]) }
 
     it 'has a King' do
       expect(white_king).to be_instance_of King
     end
 
     it 'can move one square any direction' do
-      white_king.move([4,1])
-      expect(white_king.position).to eq([4,1])
-      white_king.move([4,2])
-      expect(white_king.position).to eq([4,2])
-      white_king.move([5,2])
-      expect(white_king.position).to eq([5,2])
-      white_king.move([5,1])
-      expect(white_king.position).to eq([5,1])
+      expect(white_king.validate_move([5,1])).to be_truthy
+      expect(white_king.validate_move([5,3])).to be_truthy
+      expect(white_king.validate_move([4,1])).to be_truthy
+      expect(white_king.validate_move([6,2])).to be_truthy
     end
 
     it 'cannot move more than one square at a time' do
-      white_king.move([5,3])
-      expect(white_king.position).to_not eq([5,3])
+      expect(white_king.validate_move([5,4])).to be_falsey
     end
   end
 
@@ -35,43 +30,32 @@ RSpec.describe 'Pieces' do
     end
 
     it 'can move one square ahead' do
-      white_pawn.move([3,3])
-      black_pawn.move([3,6])
-      expect(white_pawn.position).to eq([3,3])
-      expect(black_pawn.position).to eq([3,6])
+      expect(white_pawn.validate_move([3,3])).to be_truthy
+      expect(black_pawn.validate_move([3,6])).to be_truthy
     end
 
     it 'cannot move more than one square at a time' do
-      black_pawn.move([3,4])
-      expect(black_pawn.position).to_not eq([3,4])
+      expect(black_pawn.validate_move([3,4])).to be_falsey
     end
 
     it 'can move two squares at a time from the initial square' do
-      white_pawn.move([3,4])
-      black_pawn.move([3,5])
-      expect(white_pawn.position).to eq([3,4])
-      expect(black_pawn.position).to eq([3,5])
+      expect(white_pawn.validate_move([3,4])).to be_truthy
+      expect(black_pawn.validate_move([3,5])).to be_truthy
     end
 
     it 'cannot move to the left or rigth' do
-      white_pawn.move([4,2])
-      black_pawn.move([2,6])
-      expect(white_pawn.position).to_not eq([4,2])
-      expect(black_pawn.position).to_not eq([2,6])
+      expect(white_pawn.validate_move([4,2])).to be_falsey
+      expect(black_pawn.validate_move([2,6])).to be_falsey
     end
 
     it 'cannot move back' do
-      white_pawn.move([3,1])
-      black_pawn.move([3,8])
-      expect(white_pawn.position).to_not eq([3,1])
-      expect(black_pawn.position).to_not eq([3,8])
+      expect(white_pawn.validate_move([3,1])).to be_falsey
+      expect(black_pawn.validate_move([3,8])).to be_falsey
     end
 
     it 'can attack diagonally' do
-      white_pawn.move([2,3], :attack)
-      black_pawn.move([2,6], :attack)
-      expect(white_pawn.position).to eq([2,3])
-      expect(black_pawn.position).to eq([2,6])
+      expect(white_pawn.validate_move([2,3], true)).to be_truthy
+      expect(black_pawn.validate_move([2,6], true)).to be_truthy
     end
   end
 
@@ -84,20 +68,16 @@ RSpec.describe 'Pieces' do
     end
 
     it 'can move any number of squares horizontally' do
-      white_rook.move([1,4])
-      expect(white_rook.position).to eq([1,4])
+      expect(white_rook.validate_move([1,4])).to be_truthy
     end
 
     it 'can move vertically' do
-      black_rook.move([4,1])
-      expect(black_rook.position).to eq([4,1])
+      expect(black_rook.validate_move([4,1])).to be_truthy
     end
 
     it 'cannot move diagonally or in any other way' do
-      white_rook.move([2,8])
-      black_rook.move([5,6])
-      expect(white_rook.position).to_not eq([2,8])
-      expect(black_rook.position).to_not eq([5,6])
+      expect(white_rook.validate_move([2,8])).to be_falsey
+      expect(black_rook.validate_move([5,6])).to be_falsey
     end
   end
 
@@ -110,15 +90,12 @@ RSpec.describe 'Pieces' do
     end
 
     it 'it can move diagonally' do
-      white_bishop.move([7,2])
-      expect(white_bishop.position).to eq([7,2])
+      expect(white_bishop.validate_move([7,2])).to be_truthy
     end
 
     it 'cannot move horizontally or vertically' do
-      white_bishop.move([5,6])
-      black_bishop.move([2,6])
-      expect(white_bishop.position).to_not eq([5,6])
-      expect(black_bishop.position).to_not eq([2,6])
+      expect(white_bishop.validate_move([5,6])).to be_falsey
+      expect(black_bishop.validate_move([2,6])).to be_falsey
     end
   end
 
@@ -130,13 +107,11 @@ RSpec.describe 'Pieces' do
     end
 
     it 'jumps two squares up and one to the side' do
-      white_knight.move([7,3])
-      expect(white_knight.position).to eq([7,3])
+      expect(white_knight.validate_move([7,3])).to be_truthy
     end
 
     it 'cannot jump to a square outside of it\'s reach' do
-      white_knight.move([2,3])
-      expect(white_knight.position).to_not eq([2,3])
+      expect(white_knight.validate_move([2,3])).to be_falsey
     end
   end
 
@@ -152,23 +127,19 @@ RSpec.describe 'Pieces' do
     end
 
     it 'can move horizontally' do
-      white_queen.move([8,4])
-      expect(white_queen.position).to eq([8,4])
+      expect(white_queen.validate_move([8,4])).to be_truthy
     end
 
     it 'can move vertically' do
-      white_queen.move([4,1])
-      expect(white_queen.position).to eq([4,1])
+      expect(white_queen.validate_move([4,1])).to be_truthy
     end
 
     it 'can move diagonally' do
-      white_queen.move([1,7])
-      expect(white_queen.position).to eq([1,7])
+      expect(white_queen.validate_move([1,7])).to be_truthy
     end
 
     it 'cannot move in but a straight line' do
-      white_queen.move([5,6])
-      expect(white_queen.position).to_not eq([5,6])
+      expect(white_queen.validate_move([5,6])).to be_falsey
     end
   end
 end
