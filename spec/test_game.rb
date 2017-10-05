06bd4daca100f809a2afb76205e2cd19e939b62c
andrewr224 <andrew.r224@gmail.com>
2017-10-05 10:58:42 +0300

    Delete test_game files

--- spec/test_game.rb
+++
@@ -1,7 +0,0 @@
-#RSpec.describe 'Test game' do
-  #$stdin = File.open('./spec/test_game.txt', 'r')
-  #let(:test_game) { Chess.new }
-  #it 'allwos two players to play a game' do
-    #expect(test_game.play).to raise_error
-  #end
-#end
